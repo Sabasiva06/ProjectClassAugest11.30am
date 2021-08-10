81daf872171c452c1ce09e399dc45f46feb4e333
Sabasiva06 <sabasiva06@gmail.com>
2021-08-10 11:56:40 +0530

    augst work

--- src/test/java/org/test/TestRunner.java
+++ src/test/java/org/test/TestRunner.java
@@ -6,9 +6,27 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"D:\\CucumberAfternoonBatch\\src\\test\\resources\\facebook.feature"})
+@CucumberOptions(features = { "D:\\CucumberAfternoonBatch\\src\\test\\resources\\facebook.feature" })
 public class TestRunner {
 
-	
-	
+	public void a1() {
+		System.out.println("a1");
+
+	}
+
+	public void a2() {
+		System.out.println("a2");
+
+	}
+
+	public void a3() {
+		System.out.println("a3");
+
+	}
+
+	public void a4() {
+		System.out.println("a4");
+
+	}
+
 }
